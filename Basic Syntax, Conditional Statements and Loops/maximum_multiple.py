56af4ced461bc0c101909c50e5ce05908d33fb73
Alexander Zashev <azashev93@gmail.com>
2022-05-21 05:14:03 +0300

    ~

--- Basic Syntax, Conditional Statements and Loops/maximum_multiple.py
+++ Basic Syntax, Conditional Statements and Loops/maximum_multiple.py
@@ -1,10 +1,10 @@
 divisor = int(input())
 boundary = int(input())
 
-max_num = 0
-
-for num in range(1, boundary + 1):
+for num in range(boundary, divisor, - 1):
     if num % divisor == 0:
-        max_num = num
+        print(num)
+        break
 
-print(max_num)
+
+#print(boundary // divisor * divisor)
