48b282a2dca7534c16aa90fde9fa7c2417508ea5
Alexander Zashev <azashev93@gmail.com>
2022-05-28 14:22:40 +0300

    ~

--- Data Types and Variables/next_happy_year.py
+++ Data Types and Variables/next_happy_year.py
@@ -1,3 +1,12 @@
 n = int(input())
 
-for i in range(len(str(n))):
+while True:
+    n += 1
+    year = ''
+
+    for num in str(n):
+        year += num
+
+    if len(set(year)) == len(str(n)):
+        print(int(year))
+        break
