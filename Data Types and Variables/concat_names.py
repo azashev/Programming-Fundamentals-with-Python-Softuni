33a407c58f0aa8191071dc490d7ad1b5eacd8049
Alexander Zashev <azashev93@gmail.com>
2022-05-28 03:36:38 +0300

    ~

---
+++ Data Types and Variables/concat_names.py
@@ -0,0 +1,7 @@
+first_name = input()
+second_name = input()
+delimiter = input()
+
+result = first_name + delimiter + second_name
+
+print(result)
---
+++ Data Types and Variables/convert_meters_to_kilometers.py
@@ -0,0 +1,5 @@
+meters = float(input())
+
+kilometers = meters / 1000
+
+print(f"{kilometers:.2f}")
