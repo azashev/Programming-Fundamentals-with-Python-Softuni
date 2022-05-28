963a32d475e1d5590f514aeb70f21f9a191d059f
Alexander Zashev <azashev93@gmail.com>
2022-05-28 14:25:57 +0300

    ~

--- Data Types and Variables/special_numbers.py
+++ Data Types and Variables/special_numbers.py
@@ -1,4 +1,12 @@
 n = int(input())
 
 for i in range(1, n + 1):
-    for num in 
+    sum_digits = 0
+
+    for num in str(i):
+        sum_digits += int(num)
+
+    if sum_digits == 5 or sum_digits == 7 or sum_digits == 11:
+        print(f"{i} -> True")
+    else:
+        print(f"{i} -> False")
