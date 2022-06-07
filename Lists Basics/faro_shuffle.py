5adca0e83ed26d13d1412853a17549b27d9be882
Alexander Zashev <azashev93@gmail.com>
2022-06-08 02:57:29 +0300

    ~

---
+++ Lists Basics/faro_shuffle.py
@@ -0,0 +1,14 @@
+string = input().split()
+shuffles = int(input())
+
+for shuffles in range(shuffles):
+    left_half = string[:(len(string) // 2):]
+    right_half = string[(len(string) // 2)::]
+    current_list = []
+
+    for i in range(len(string) // 2):
+        current_list.append(left_half[i])
+        current_list.append(right_half[i])
+    string = current_list
+
+print(string)
