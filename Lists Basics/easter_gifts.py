47a4d2e8dc588c92eaffc57311e19d67c67b0bf1
Alexander Zashev <azashev93@gmail.com>
2022-06-09 20:20:08 +0300

    ~

--- Lists Basics/easter_gifts.py
+++ Lists Basics/easter_gifts.py
@@ -1,16 +1,27 @@
 gifts = input().split()
 
-command = input().split()
+command = input()
 
 while command != "No Money":
-    if len(command) > 0:
-        gift = command[1]
+    split_command = command.split()
+    if len(command) > 1:
+        gift = split_command[1]
 
-        if len(command) == 2:
-            index = int(command[2])
+        if len(split_command) == 3:
+            index = int(split_command[2])
             if 0 <= index <= len(gifts):
                 gifts[index] = gift
 
         else:
-            if command[0] == "OutOfStock":
-                
+            if split_command[0] == "OutOfStock":
+                for element in range(len(gifts)):
+                    if gifts[element] == gift:
+                        gifts[element] = None
+            if split_command[0] == "JustInCase":
+                gifts[-1] = gift
+
+    command = input()
+
+for _ in gifts:
+    if _ is not None:
+        print(_, end=" ")
