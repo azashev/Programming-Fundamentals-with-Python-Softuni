c9e7ad1136ab212028bdc65ca546b2e773d648cc
Alexander Zashev <azashev93@gmail.com>
2022-06-12 17:33:16 +0300

    ~

---
+++ Functions/smallest_of_three_numbers.py
@@ -0,0 +1,9 @@
+def smallest_number(number_one, number_two, number_three):
+    return min(number_one, number_two, number_three)
+
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+
+print(smallest_number(first_number, second_number, third_number))
