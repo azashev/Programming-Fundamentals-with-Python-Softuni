5a6b103b50c46b0efd5d00afceb17b22b4f620d6
Alexander Zashev <azashev93@gmail.com>
2022-07-01 17:26:11 +0300

    ~

--- Classes and Objects/town.py
+++ Classes and Objects/town.py
@@ -13,6 +13,7 @@ class Town:
     def __repr__(self):
         return f"Town: {self.name} | Latitude: {self.latitude} | Longitude: {self.longitude}"
 
+
 town = Town("Sofia")
 town.set_latitude("42° 41\' 51.04\" N")
 town.set_longitude("23° 19\' 26.94\" E")
