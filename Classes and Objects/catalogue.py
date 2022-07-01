b2c4ed55cad1b34b7af15e497139c59a27b76052
Alexander Zashev <azashev93@gmail.com>
2022-07-01 16:53:32 +0300

    ~

---
+++ Classes and Objects/catalogue.py
@@ -0,0 +1,24 @@
+class Catalogue:
+    def __init__(self, name):
+        self.name = name
+        self.products = []
+
+    def add_product(self, product_name: str):
+        self.products.append(product_name)
+
+    def get_by_letter(self, first_letter: str):
+        return [x for x in self.products if x.startswith(first_letter)]
+
+    def __repr__(self):
+        self.separator = '\n'
+        return f"Items in the {self.name} catalogue:\n{self.separator.join(sorted(self.products))}"
+
+
+catalogue = Catalogue("Furniture")
+catalogue.add_product("Sofa")
+catalogue.add_product("Mirror")
+catalogue.add_product("Desk")
+catalogue.add_product("Chair")
+catalogue.add_product("Carpet")
+print(catalogue.get_by_letter("C"))
+print(catalogue)
