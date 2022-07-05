6856cd87831c02b6a04ca5a4b7904bf696578ac9
Alexander Zashev <azashev93@gmail.com>
2022-07-05 22:42:53 +0300

    ~

--- Dictionaries/stock_02.py
+++ Dictionaries/stock_02.py
@@ -1,3 +1,8 @@
+# You will be given key-value pairs of products and quantities (on a single line separated by space).
+# On the following line, you will be given products to search for. Check for each product. You have 2 possibilities:
+# •	If you have the product, print "We have {quantity} of {product} left".
+# •	Otherwise, print "Sorry, we don't have {product}".
+
 products_input = input().split()
 products_to_search = input().split()
 
