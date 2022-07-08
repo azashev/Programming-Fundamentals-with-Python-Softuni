0c70dadf8a421e5acaa1bde3355a7a7a644e3a40
Alexander Zashev <azashev93@gmail.com>
2022-07-08 18:22:24 +0300

    ~

---
+++ Dictionaries/count_chars_in_a_string_01.py
@@ -0,0 +1,15 @@
+# Write a program that counts all characters in a string except for space (" ")
+# Print all the occurrences in the following format: "{char} -> {occurrences}"
+
+words = input().replace(" ", "")
+
+my_dict = {}
+
+for char in words:
+    if char not in my_dict:
+        my_dict[char] = 1
+    else:
+        my_dict[char] += 1
+
+for word, value in my_dict.items():
+    print(f"{word} -> {value}")
