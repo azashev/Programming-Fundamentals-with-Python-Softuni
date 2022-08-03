c3570b8d806fd30c1dc5b6dd59446a57ec3e13d1
Alexander Zashev <azashev93@gmail.com>
2022-08-03 21:28:45 +0300

    ~

--- Final Exam/world_tour_01.py
+++ Final Exam/world_tour_01.py
@@ -33,3 +33,24 @@ while True:
             print(stops)
 
 print(f"Ready for world tour! Planned stops: {stops}")
+
+
+# On the first line, you will be given a string containing all of your stops.
+# Until you receive the command "Travel", you will be given some commands to manipulate that initial string.
+#
+# The commands can be:
+# • "Add Stop:{index}:{string}":
+#   - Insert the given string at that index only if the index is valid
+# • "Remove Stop:{start_index}:{end_index}":
+#   - Remove the elements of the string from the starting index to the end index (inclusive) if both indices are valid
+# • "Switch:{old_string}:{new_string}":
+#   - If the old string is in the initial string, replace it with the new one (all occurrences)
+#
+# Note: After each command, print the current state of the string if it is valid!
+# After the "Travel" command, print the following: "Ready for world tour! Planned stops: {string}"
+# Input / Constraints
+# • JavaScript: you will receive a list of strings
+# • An index is valid if it is between the first and the last element index (inclusive) (0 ….. Nth) in the sequence.
+#
+# Output:
+# •	Print the proper output messages in the proper cases as described in the problem description
